d2d60f165baf9b6d89cef4722248dafa5be99f0c
dickensallinone <dickensallinone@gmail.com>
2023-11-30 07:32:42 +0800

    applying intersection observer api for showing sections of the static website

--- script/main.js
+++ script/main.js
@@ -1,8 +1,11 @@
 "use strict";
-
+//nav section
 const navItemsMenu = document.querySelector("#menu");
 const closeNavBtn = document.querySelector("#close-nav");
 const navbarItems = document.querySelector('#navbar-items');
+//
+
+const pageSections = document.querySelectorAll(".page-section");
 
 //navbar functionality
 navItemsMenu.addEventListener("click",()=>{
@@ -25,10 +28,12 @@ closeNavBtn.addEventListener("click",
 })
 
 window.addEventListener("load",()=>{
-    if(navbarItems.classList.contains("visible")) {
+    if(navbarItems.classList.contains("visible")
+    && window.innerWidth <=600) {
         navbarItems.classList.remove("visible");
         navbarItems.classList.add("hidden");
     }
+
 })
 
 window.addEventListener("resize",(e)=>{
@@ -62,4 +67,23 @@ window.addEventListener("resize",(e)=>{
 //end of navbar functionality
 //will refactor the code as soon as possible
 
-//
+//section load
+
+// pageSections.forEach(val=>val.classList.add("hidden"))
+
+let options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: .25,
+  };
+
+  let callback = (entries, observer) => {
+    const entry = entries[0];
+
+    console.log(entry.target);
+  };
+  
+  let observer = new IntersectionObserver(callback, options);
+
+  pageSections.forEach(val=>
+    observer.observe(val));
